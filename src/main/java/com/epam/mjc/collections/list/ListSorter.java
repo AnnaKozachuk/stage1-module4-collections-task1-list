f6e684620f426af90f1138d35dc8dc0f0822c4ce
FlyingElephant135 <46475082+FlyingElephant135@users.noreply.github.com>
2023-10-24 20:36:27 +0300

    commit

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,20 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aValue = Integer.parseInt(a);
+        int bValue = Integer.parseInt(b);
+        int aFunctionValue = 5 * aValue * aValue + 3;
+        int bFunctionValue = 5 * bValue * bValue + 3;
+        if (aFunctionValue != bFunctionValue) {
+            return Integer.compare(aFunctionValue, bFunctionValue);
+        }
+        return Integer.compare(aValue, bValue);
     }
 }
